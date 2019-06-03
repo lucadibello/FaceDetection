83a9d4f60a385bde54f6b2207cdb97c18761cb25
Luca Di Bello <luca.dibello@samtrevano.ch>
2019-06-03 15:46:35 +0100

    Added comments

--- face_detection.py
+++ face_detection.py
@@ -1,21 +1,28 @@
 import cv2
 
+# Load xml file for face recognition
 haar_cascade_face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 def show_webcam(mirror=False):
     cam = cv2.VideoCapture(0)
     
     while True:
+        # Read webcam frame
         ret_val, img = cam.read()
 
         # Show image
         img = face_detection(img, width = 10)
 
         if mirror:
+            # Flip webcam image (mirror)
             img = cv2.flip(img, 1)
+
+        # Show webcam output in a window
         cv2.imshow('Hello there', img)
+        
+        # Set exit key (esc to quit)
         if cv2.waitKey(1) == 27: 
-            break  # esc to quit
+            break
 
     cv2.destroyAllWindows()
 
@@ -23,14 +30,18 @@ def face_detection(img, scaleFactor= 1.1, width = 15):
     # Convert to gray scale
     img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    faces_rects = haar_cascade_face.detectMultiScale(img_grey, scaleFactor = scaleFactor, minNeighbors = 5);    
+    # Detect faces
+    faces = haar_cascade_face.detectMultiScale(img_grey, scaleFactor = scaleFactor, minNeighbors = 5);    
 
-    for (x, y, w, h) in faces_rects:
+    for (x, y, w, h) in faces:
+        # Draw rects
         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), width)
 
+    # Return image with rects
     return img
 
 def main():
+    # Start webcam
     show_webcam(mirror=True)
 
 if __name__ == "__main__":
